2bcca65bc964269fb608b5adc919b72f1c633631
Haznto <haznto@gmail.com>
2023-04-18 11:09:13 +0000

    adding forms and Events , getting information directly from the user and create employee card based on the information

--- js/app.js
+++ js/app.js
@@ -22,13 +22,13 @@ Employee.prototype.SalaryCalc = function() {
         return Salary;
 }
     }
-    let ghazi = new Employee(1000,'Ghazi Samer','Administration','Senior','https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720/76729750.jpg')
-    let lana = new Employee(1001,'Lana Ali','Finance','Senior','https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720/76729750.jpg')
-    let tamara = new Employee(1002,'Tamara Ayoub','Marketing','Senior','https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720/76729750.jpg')
-    let safi = new Employee(1003,'Safi Walid','Administration','Mid-Senior','https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720/76729750.jpg')
-    let omar = new Employee(1004,'Omar Zaid','Development','Senior','https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720/76729750.jpg')
-    let rana = new Employee(1005,'Rana Saleh','Development','Junior','https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720/76729750.jpg')
-    let hadi = new Employee(1006,'Hadi Ahmad','Finance','Mid-Senior','https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720/76729750.jpg')
+    let ghazi = new Employee(1000,'Ghazi Samer','Administration','Senior','https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Ghazi.jpg?raw=true')
+    let lana = new Employee(1001,'Lana Ali','Finance','Senior','https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Lana.jpg?raw=true')
+    let tamara = new Employee(1002,'Tamara Ayoub','Marketing','Senior','https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Tamara.jpg?raw=true')
+    let safi = new Employee(1003,'Safi Walid','Administration','Mid-Senior','https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Safi.jpg?raw=true')
+    let omar = new Employee(1004,'Omar Zaid','Development','Senior','https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Omar.jpg?raw=true')
+    let rana = new Employee(1005,'Rana Saleh','Development','Junior','https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Rana.jpg?raw=true')
+    let hadi = new Employee(1006,'Hadi Ahmad','Finance','Mid-Senior','https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Hadi.jpg?raw=true')
     
 
 Employee.prototype.renderToHomePage = function() {
@@ -57,11 +57,12 @@ Employee.prototype.renderToHomePage = function() {
         employeeNum =7;
         break;
     }
-    
+    let employeeImg = document.getElementById('eImg'+employeeNum)
     let employeeName = document.getElementById('employee'+employeeNum)
     let employeeSalary = document.getElementById('employeeSalary'+employeeNum)
-    employeeName.textContent = this.fullName;
-    employeeSalary.textContent = this.salary;
+    employeeName.textContent = `Name:${this.fullName} --- ID:${this.employeeID}`;
+    employeeSalary.textContent = `Department: ${this.depratnment} --- Level: ${this.level} Total Salary after Tax: $${this.salary}`;
+    employeeImg.src = this.img;
 }
 ghazi.renderToHomePage()
 lana.renderToHomePage()
@@ -71,3 +72,149 @@ omar.renderToHomePage()
 rana.renderToHomePage()
 hadi.renderToHomePage()
 
+
+
+// updating javascirpt for assignment lab 8 for this project.
+
+//creating random ID number 4 digits
+const genRandomNum =  () => {
+let randomNum = Math.floor(1000 +Math.random()*9000);
+console.log(randomNum);
+return randomNum;
+
+}
+// consturctior function to save data we takefrom the function.
+
+function makeCard(fullName,department,level,urlImg) {
+    this.fullName = fullName;
+    this.department = department;
+    this.level = level;
+    this.urlImg = urlImg;
+    this.employeeID =  genRandomNum();
+}
+
+// creating prototypes for each makeCard object:
+
+makeCard.prototype.renderInHome = function () {
+    if (this.fullName !== null && this.fullName !== "" && this.urlImg !== null && this.urlImg !== "") {
+        if (this.department === 'Administration') {
+            const intoCard = document.getElementById('administrationSec');
+
+            const createH2 = document.createElement('h2');
+        createH2.textContent = this.fullName
+        const createPara = document.createElement('p');
+        // createPara.innerHTML = `Department: ${this.department}` `level: ${this.level}` `ID: ${this.employeeID}`
+        createPara.textContent = `Department:${this.department} level:${this.level} ID:${this.employeeID}`;
+
+        const creatingImg = document.createElement('img');
+        creatingImg.src = this.urlImg;
+        const creatingDiv = document.createElement('div');   
+        intoCard.appendChild(creatingDiv);
+        creatingDiv.append(creatingImg,createH2,createPara);
+        }
+        else if (this.department === 'Finance') {
+            const intoCard = document.getElementById('financeSec');
+
+            const createH2 = document.createElement('h2');
+        createH2.textContent = this.fullName
+        const createPara = document.createElement('p');
+        // createPara.innerHTML = `Department: ${this.department}` `level: ${this.level}` `ID: ${this.employeeID}`
+        createPara.textContent = `Department:${this.department} level:${this.level} ID:${this.employeeID}`;
+
+        const creatingImg = document.createElement('img');
+        creatingImg.src = this.urlImg;
+        const creatingDiv = document.createElement('div');   
+        intoCard.appendChild(creatingDiv);
+        creatingDiv.append(creatingImg,createH2,createPara);
+        }
+        else if (this.department === 'Development') {
+            const intoCard = document.getElementById('developmentSec');
+
+            const createH2 = document.createElement('h2');
+        createH2.textContent = this.fullName
+        const createPara = document.createElement('p');
+        // createPara.innerHTML = `Department: ${this.department}` `level: ${this.level}` `ID: ${this.employeeID}`
+        createPara.textContent = `Department:${this.department} level:${this.level} ID:${this.employeeID}`;
+
+        const creatingImg = document.createElement('img');
+        creatingImg.src = this.urlImg;
+        const creatingDiv = document.createElement('div');   
+        intoCard.appendChild(creatingDiv);
+        creatingDiv.append(creatingImg,createH2,createPara);
+        }
+        else if (this.department === 'Marketing') {
+            const intoCard = document.getElementById('marketingSec');
+
+            const createH2 = document.createElement('h2');
+        createH2.textContent = this.fullName
+        const createPara = document.createElement('p');
+        // createPara.innerHTML = `Department: ${this.department}` `level: ${this.level}` `ID: ${this.employeeID}`
+        createPara.textContent = `Department:${this.department} level:${this.level} ID:${this.employeeID}`;
+
+        const creatingImg = document.createElement('img');
+        creatingImg.src = this.urlImg;
+        const creatingDiv = document.createElement('div');   
+        intoCard.appendChild(creatingDiv);
+        creatingDiv.append(creatingImg,createH2,createPara);
+        }
+        
+        //Template workflow for the function
+
+        // const createH2 = document.createElement('h2');
+        // createH2.textContent = this.fullName
+        // const createPara = document.createElement('p');
+        // // createPara.innerHTML = `Department: ${this.department}` `level: ${this.level}` `ID: ${this.employeeID}`
+        // createPara.textContent = `Department:${this.department} level:${this.level} ID:${this.employeeID}`;
+
+        // const creatingImg = document.createElement('img');
+        // creatingImg.src = this.urlImg;
+        // const creatingDiv = document.createElement('div');   
+        // intoCard.appendChild(creatingDiv);
+        // creatingDiv.append(creatingImg,createH2,createPara);
+    }
+    // const creatingImg = document.createElement('img');
+    // creatingImg.textContent = "This is Me"
+    // const createH2 = document.createElement('h2');
+    // createH2.textContent = this.fullName
+    // const createPara = document.createElement('p');
+    // createPara.textContent = `Department: ${this.department} level: ${this.level} ID: ${this.employeeID}`
+    // intoCard.appendChild(createPara)
+
+    // const divFinal = creatingDiv.append(createH2,createPara,creatingImg);
+    // intoCard.append(divFinal);
+
+
+
+
+}
+
+//creating function to invoke when invoking the event
+
+function gettingInfo(formInfoEvent) {
+    formInfoEvent.preventDefault();
+    console.log(formInfoEvent)
+    // debugger
+    let fullName = formInfoEvent.target.fullName.value;
+    let department = formInfoEvent.target.department.value;
+    let level = formInfoEvent.target.level.value;
+    let urlImg = formInfoEvent.target.urlImg.value;
+
+
+    const newEmployee = new makeCard(fullName,department,level,urlImg)
+    console.log(newEmployee)
+
+    newEmployee.renderInHome()
+    // console.log(fullName,department,level,urlImg)
+}
+
+
+
+
+
+
+
+
+
+const agentForm = document.getElementById("employeeForm");
+agentForm.addEventListener('submit', gettingInfo)
+console.log(agentForm)
